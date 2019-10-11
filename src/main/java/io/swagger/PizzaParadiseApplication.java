787bc78393c8c642ce7220c68d6c86b7f6462b7b
yejeelee <lee.yej@husky.neu.edu>
2019-10-10 23:54:12 -0700

    uploading all necessary working file

---
+++ src/main/java/io/swagger/PizzaParadiseApplication.java
@@ -0,0 +1,17 @@
+package io.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = { "io.swagger", "io.swagger.api" , "io.swagger.configuration"})
+public class PizzaParadiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaParadiseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/io/swagger/model/SpecialItem.java
@@ -0,0 +1,134 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.UUID;
+import org.threeten.bp.OffsetDateTime;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * SpecialItem
+ */
+
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-09-26T03:54:46.062Z[GMT]")
+
+public class SpecialItem {
+  @JsonProperty("id")
+  private UUID id = null;
+
+  @JsonProperty("name")
+  private String name = null;
+
+  @JsonProperty("releaseDate")
+  private OffsetDateTime releaseDate = null;
+
+  public SpecialItem id(UUID id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+   **/
+  @ApiModelProperty(example = "d290f1ee-6c54-4b01-90e6-d701748f0851", required = true, value = "")
+  @NotNull
+
+  @Valid
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public SpecialItem name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+   **/
+  @ApiModelProperty(example = "Widget Adapter", required = true, value = "")
+  @NotNull
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public SpecialItem releaseDate(OffsetDateTime releaseDate) {
+    this.releaseDate = releaseDate;
+    return this;
+  }
+
+  /**
+   * Get releaseDate
+   * @return releaseDate
+   **/
+  @ApiModelProperty(example = "2019-10-04T09:12:33.001Z", required = true, value = "")
+  @NotNull
+
+  @Valid
+  public OffsetDateTime getReleaseDate() {
+    return releaseDate;
+  }
+
+  public void setReleaseDate(OffsetDateTime releaseDate) {
+    this.releaseDate = releaseDate;
+  }
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    SpecialItem specialItem = (SpecialItem) o;
+    return Objects.equals(this.id, specialItem.id) &&
+        Objects.equals(this.name, specialItem.name) &&
+        Objects.equals(this.releaseDate, specialItem.releaseDate);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, releaseDate);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class SpecialItem {\n");
+
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    releaseDate: ").append(toIndentedString(releaseDate)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
