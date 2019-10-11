bab0f5e314f83818a6b16b3f5761b8801233abcb
yejeelee <lee.yej@husky.neu.edu>
2019-10-11 01:03:50 -0700

    Added necessary files

---
+++ src/main/java/io/swagger/api/ToppingApiController.java
@@ -0,0 +1,59 @@
+package io.swagger.api;
+
+import io.swagger.model.ToppingItem;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-09-26T03:54:46.062Z[GMT]")
+@Controller
+public class ToppingApiController implements ToppingApi {
+
+  private static final Logger log = LoggerFactory.getLogger(ToppingApiController.class);
+
+  private final ObjectMapper objectMapper;
+
+  private final HttpServletRequest request;
+
+  @org.springframework.beans.factory.annotation.Autowired
+  public ToppingApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+    this.objectMapper = objectMapper;
+    this.request = request;
+  }
+
+  public ResponseEntity<Void> addTopping(@ApiParam(value = "Topping item to add"  )  @Valid @RequestBody ToppingItem body) {
+    String accept = request.getHeader("Accept");
+    return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+  }
+
+  public ResponseEntity<List<ToppingItem>> searchTopping(@ApiParam(value = "pass an optional search string for looking up toppings") @Valid @RequestParam(value = "searchString", required = false) String searchString,@Min(0)@ApiParam(value = "number of records to skip for pagination", allowableValues = "") @Valid @RequestParam(value = "skip", required = false) Integer skip,@Min(0) @Max(50) @ApiParam(value = "maximum number of records to return", allowableValues = "") @Valid @RequestParam(value = "limit", required = false) Integer limit) {
+    String accept = request.getHeader("Accept");
+    if (accept != null && accept.contains("application/json")) {
+      try {
+        return new ResponseEntity<List<ToppingItem>>(objectMapper.readValue("[ {\n  \"releaseDate\" : \"2016-08-29T09:12:33.001Z\",\n  \"name\" : \"Widget Adapter\",\n  \"id\" : \"d290f1ee-6c54-4b01-90e6-d701748f0851\"\n}, {\n  \"releaseDate\" : \"2016-08-29T09:12:33.001Z\",\n  \"name\" : \"Widget Adapter\",\n  \"id\" : \"d290f1ee-6c54-4b01-90e6-d701748f0851\"\n} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
+      } catch (IOException e) {
+        log.error("Couldn't serialize response for content type application/json", e);
+        return new ResponseEntity<List<ToppingItem>>(HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    }
+
+    return new ResponseEntity<List<ToppingItem>>(HttpStatus.NOT_IMPLEMENTED);
+  }
+
+}
---
+++ src/main/java/io/swagger/model/ToppingItem.java
@@ -0,0 +1,133 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.UUID;
+import org.threeten.bp.OffsetDateTime;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * ToppingItem
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-09-26T03:54:46.062Z[GMT]")
+public class ToppingItem   {
+  @JsonProperty("id")
+  private UUID id = null;
+
+  @JsonProperty("name")
+  private String name = null;
+
+  @JsonProperty("releaseDate")
+  private OffsetDateTime releaseDate = null;
+
+  public ToppingItem id(UUID id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+   **/
+  @ApiModelProperty(example = "d290f1ee-6c54-4b01-90e6-d701748f0851", required = true, value = "")
+  @NotNull
+
+  @Valid
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public ToppingItem name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+   **/
+  @ApiModelProperty(example = "Widget Adapter", required = true, value = "")
+  @NotNull
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public ToppingItem releaseDate(OffsetDateTime releaseDate) {
+    this.releaseDate = releaseDate;
+    return this;
+  }
+
+  /**
+   * Get releaseDate
+   * @return releaseDate
+   **/
+  @ApiModelProperty(example = "2016-08-29T09:12:33.001Z", required = true, value = "")
+  @NotNull
+
+  @Valid
+  public OffsetDateTime getReleaseDate() {
+    return releaseDate;
+  }
+
+  public void setReleaseDate(OffsetDateTime releaseDate) {
+    this.releaseDate = releaseDate;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ToppingItem toppingItem = (ToppingItem) o;
+    return Objects.equals(this.id, toppingItem.id) &&
+        Objects.equals(this.name, toppingItem.name) &&
+        Objects.equals(this.releaseDate, toppingItem.releaseDate);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, releaseDate);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ToppingItem {\n");
+
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    releaseDate: ").append(toIndentedString(releaseDate)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
