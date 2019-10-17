9a87db080f75b010a689f5f17f70f98dae56f70d
Clara Mae Wells <wells.cl@husky.neu.edu>
2019-10-16 23:50:58 -0700

    add DELETE GET{id} operations, update GET POST operations

--- src/main/java/io/swagger/api/StoreApi.java
+++ src/main/java/io/swagger/api/StoreApi.java
@@ -6,74 +6,23 @@ import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @javax.annotation.Generated(
     value = "io.swagger.codegen.v3.generators.java.SpringCodegen",
     date = "2019-09-26T03:54:46.062Z[GMT]")
-@Api(value = "store", description = "the store API")
+@Api(value = "stores", description = "the store API")
 public interface StoreApi {
+  // Get all stores
+  ResponseEntity<List<StoreItem>> getAllStores();
 
-  @ApiOperation(
-      value = "adds a Store item",
-      nickname = "addStore",
-      notes = "Adds a Store to the system",
-      tags = {
-        "store",
-      })
-  @ApiResponses(
-      value = {
-        @ApiResponse(code = 201, message = "store created"),
-        @ApiResponse(code = 400, message = "invalid input, object invalid"),
-        @ApiResponse(code = 409, message = "an existing item already exists")
-      })
-  @RequestMapping(
-      value = "/store",
-      consumes = {"application/json"},
-      method = RequestMethod.POST)
-  ResponseEntity<Void> addStore(
-      @ApiParam(value = "Store item to add") @Valid @RequestBody StoreItem body);
+  // Get a specific StoreItem by id
+  ResponseEntity<StoreItem> getStoreById(@PathVariable String id);
 
-  @ApiOperation(
-      value = "searches for store",
-      nickname = "searchStore",
-      notes =
-          "By passing in the appropriate options, you can search for available stores in the system ",
-      response = StoreItem.class,
-      responseContainer = "List",
-      tags = {
-        "store",
-      })
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            code = 200,
-            message = "search results matching criteria",
-            response = StoreItem.class,
-            responseContainer = "List"),
-        @ApiResponse(code = 400, message = "bad input parameter")
-      })
-  @RequestMapping(
-      value = "/store",
-      produces = {"application/json"},
-      method = RequestMethod.GET)
-  ResponseEntity<List<StoreItem>> searchStore(
-      @ApiParam(value = "pass an optional search string for looking up stores")
-          @Valid
-          @RequestParam(value = "searchString", required = false)
-          String searchString,
-      @Min(0)
-          @ApiParam(value = "number of records to skip for pagination", allowableValues = "")
-          @Valid
-          @RequestParam(value = "skip", required = false)
-          Integer skip,
-      @Min(0)
-          @Max(50)
-          @ApiParam(value = "maximum number of records to return", allowableValues = "")
-          @Valid
-          @RequestParam(value = "limit", required = false)
-          Integer limit);
-}
+  // Add a StoreItem
+  ResponseEntity<StoreItem> addStore(
+      @ApiParam(value = "StoreItem to add") @Valid @RequestBody StoreItem newStore);
+
+  // Delete a StoreItem by id
+  ResponseEntity<String> deleteStore(@PathVariable String id);
--- src/main/java/io/swagger/api/StoreApiController.java
+++ src/main/java/io/swagger/api/StoreApiController.java
@@ -1,75 +1,79 @@
 package io.swagger.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
-import io.swagger.model.StoreItem;
-import java.io.IOException;
+import io.swagger.model.SpecialItem;
+import io.swagger.repository.SpecialItemRepository;
+import io.swagger.service.SpecialItemService;
+
 import java.util.List;
-import javax.servlet.http.HttpServletRequest;
+
 import javax.validation.Valid;
-import javax.validation.constraints.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @javax.annotation.Generated(
     value = "io.swagger.codegen.v3.generators.java.SpringCodegen",
     date = "2019-09-26T03:54:46.062Z[GMT]")
 @Controller
 public class StoreApiController implements StoreApi {
+  @Autowired
+  private StoreItemService storeItemService;
 
-  private static final Logger log = LoggerFactory.getLogger(StoreApiController.class);
-
-  private final ObjectMapper objectMapper;
-
-  private final HttpServletRequest request;
-
-  @org.springframework.beans.factory.annotation.Autowired
-  public StoreApiController(ObjectMapper objectMapper, HttpServletRequest request) {
-    this.objectMapper = objectMapper;
-    this.request = request;
+  @GetMapping("/stores")
+  @ApiOperation(
+      value = "Get all stores",
+      tags = {
+          "store",
+      })
+  public ResponseEntity<List<StoreItem>> getAllStores() {
+    return new ResponseEntity<List<StoreItem>>(storeItemService.getAllStores(), HttpStatus.OK);
   }
 
-  public ResponseEntity<Void> addStore(
-      @ApiParam(value = "Store item to add") @Valid @RequestBody StoreItem body) {
-    String accept = request.getHeader("Accept");
-    return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
-  }
-
-  public ResponseEntity<List<StoreItem>> searchStore(
-      @ApiParam(value = "pass an optional search string for looking up stores")
-          @Valid
-          @RequestParam(value = "searchString", required = false)
-          String searchString,
-      @Min(0)
-          @ApiParam(value = "number of records to skip for pagination", allowableValues = "")
-          @Valid
-          @RequestParam(value = "skip", required = false)
-          Integer skip,
-      @Min(0)
-          @Max(50)
-          @ApiParam(value = "maximum number of records to return", allowableValues = "")
-          @Valid
-          @RequestParam(value = "limit", required = false)
-          Integer limit) {
-    String accept = request.getHeader("Accept");
-    if (accept != null && accept.contains("application/json")) {
-      try {
-        return new ResponseEntity<List<StoreItem>>(
-            objectMapper.readValue(
-                "[ {\n  \"isOpen\" : \"true\",\n  \"timeClosed\" : \"09:12:33\",\n  \"timeOpen\" : \"09:12:33\",\n  \"address\" : \"123 Main St Coastal State 77777\",\n  \"id\" : \"d290f1ee-6c54-4b01-90e6-d701748f0851\"\n}, {\n  \"releaseDate\" : \"2016-08-29T09:12:33.001Z\",\n  \"name\" : \"Widget Adapter\",\n  \"id\" : \"d290f1ee-6c54-4b01-90e6-d701748f0851\"\n} ]",
-                List.class),
-            HttpStatus.NOT_IMPLEMENTED);
-      } catch (IOException e) {
-        log.error("Couldn't serialize response for content type application/json", e);
-        return new ResponseEntity<List<StoreItem>>(HttpStatus.INTERNAL_SERVER_ERROR);
-      }
+  @GetMapping("/stores/{id}")
+  @ApiOperation(
+      value = "Get a specific StoreItem by id",
+      tags = {
+          "store",
+      })
+  public ResponseEntity<StoreItem> getStoreById(@PathVariable String id) {
+    if(storeItemService.getStoreById(id) == null) {
+      return new ResponseEntity<StoreItem>(HttpStatus.NOT_FOUND);
     }
+    return new ResponseEntity<StoreItem>(storeItemService.getStoreById(id), HttpStatus.FOUND);
+  }
 
-    return new ResponseEntity<List<StoreItem>>(HttpStatus.NOT_IMPLEMENTED);
+  @PostMapping("/stores/add")
+  @ApiOperation(
+      value = "Add a StoreItem",
+      tags = {
+          "store",
+      })
+  public ResponseEntity<StoreItem> addStore(
+      @ApiParam(value = "StoreItem to add") @Valid @RequestBody StorelItem newStore) {
+
+    return new ResponseEntity<StoreItem>(storeItemService.addStore(newStore), HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/stores/delete/{id}")
+  @ApiOperation(
+      value = "Delete a StoreItem by id",
+      tags = {
+          "store",
+      })
+  public ResponseEntity<String> deleteStore(@PathVariable String id) {
+
+    if (storeItemService.getStoreById(id) == null) {
+      return new ResponseEntity<String>("id does not exist: " + id, HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<String>(storeItemService.deleteStore(id), HttpStatus.OK);
+
   }
 }
