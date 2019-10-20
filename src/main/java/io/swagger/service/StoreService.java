95e547dd76db22786a31b7cc7e61b4657026e878
Clara Mae Wells <wells.cl@husky.neu.edu>
2019-10-19 20:13:02 -0700

    fix imports

--- src/main/java/io/swagger/service/StoreService.java
+++ src/main/java/io/swagger/service/StoreService.java
@@ -1,3 +1,8 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import javax.validation.constraints.*;
+
 public List<StoreItem> getAllStoreItems() {
   List<StoreItem> listOfStores = new ArrayList<>();
 
