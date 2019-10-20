95f5146cf1aa403819b5345303cf0bc1f5808bef
yejeelee <lee.yej@husky.neu.edu>
2019-10-20 01:29:51 -0700

    uploading to my fork

--- src/test/java/io/swagger/api/PizzaSizeApiTests.java
+++ src/test/java/io/swagger/api/PizzaSizeApiTests.java
@@ -1,36 +1,20 @@
 package io.swagger.api;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import io.swagger.PizzaParadiseApplication;
 import io.swagger.model.PizzaSize;
 import io.swagger.repository.PizzaSizeRepository;
 import io.swagger.service.PizzaSizeService;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
