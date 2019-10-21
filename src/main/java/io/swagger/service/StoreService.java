9d63f4cd80b5fee4eec7f73173dcd3c537608bfe
Clara Mae Wells <wells.cl@husky.neu.edu>
2019-10-21 00:40:32 -0700

    hardcode store to test api

--- src/main/java/io/swagger/service/StoreService.java
+++ src/main/java/io/swagger/service/StoreService.java
@@ -1,10 +1,39 @@
-package io.swagger.model;
+package io.swagger.service;
 
-import java.util.Objects;
-import javax.validation.constraints.*;
+import io.swagger.model.StoreItem;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
 
-public List<StoreItem> getAllStoreItems() {
-  List<StoreItem> listOfStores = new ArrayList<>();
+@Service
+public class StoreService {
 
-  return listOfStores;
+  public List<StoreItem> listOfStores;
+
+  public StoreService() {
+    this.listOfStores = new ArrayList<>();
+    listOfStores.add(
+        new StoreItem(
+            "9bbfc16f-d318-42f2-82f1-bf7365d4ee93",
+            "4003 Stone Way N, Seattle, Washington 98103",
+            "11:00",
+            "23:00"));
+
+  }
+  public List<StoreItem> getAllStoreItems() {
+    return listOfStores;
+  }
+
+  public StoreItem getStoreById(String id) {
+    for (StoreItem item : listOfStores) {
+      if (item.getId().equals(id)) {
+        return item;
+      }
+    }
+    return null;
+  }
+
+  public void addStoreItem(StoreItem newStore) {
+    listOfStores.add(newStore);
+  }
 }
