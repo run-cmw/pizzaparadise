98b1ce1653c76c1381a0c23f4782b78a9ab06f9d
Clara Mae Wells <wells.cl@husky.neu.edu>
2019-10-21 01:05:05 -0700

    add missing attributes to new StoreItem

--- src/main/java/io/swagger/service/StoreService.java
+++ src/main/java/io/swagger/service/StoreService.java
@@ -16,8 +16,12 @@ public class StoreService {
         new StoreItem(
             "9bbfc16f-d318-42f2-82f1-bf7365d4ee93",
             "4003 Stone Way N, Seattle, Washington 98103",
-            "11:00",
-            "23:00"));
+            "11",
+            "00"
+            null,
+            "23",
+            "00"
+            null));
 
   }
   public List<StoreItem> getAllStoreItems() {
