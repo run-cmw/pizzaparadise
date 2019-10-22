54f694d0e8b9bac7cb8177e2fe5f6480f86fb6d5
Clara Mae Wells <wells.cl@husky.neu.edu>
2019-10-22 09:55:43 -0700

    update service file w db requirements - now all files updated for db

--- src/main/java/io/swagger/service/StoreService.java
+++ src/main/java/io/swagger/service/StoreService.java
@@ -1,31 +1,32 @@
 package io.swagger.service;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.model.StoreItem;
-import java.util.ArrayList;
+import io.swagger.repository.StoreItemRepository;
 import java.util.List;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class StoreService {
+  @Autowired
+  public StoreItemRepository storeItemRepository;
 
-  public List<StoreItem> storeItems;
-
-  public StoreService() {
-    this.storeItems = new ArrayList<>();
-    storeItems.add(
-        new StoreItem(
-            "1",
-            "4003 Stone Way N",
-            "Seattle",
-            "Washington",
-            "98103"));
-  }
-  public List<StoreItem> getAllStoreItems() {
-    return storeItems;
+  public List<StoreItem> getAllStores() {
+    return storeItemRepository.findAll();
   }
 
   public StoreItem getStoreById(String id) {
-    for (StoreItem item : storeItems) {
+    for (StoreItem item : storeItemRepository.findAll()) {
       if (item.getId().equals(id)) {
         return item;
       }
@@ -33,7 +34,16 @@ public class StoreService {
     return null;
   }
 
-  public void addStoreItem(StoreItem newStore) {
-    storeItems.add(newStore);
+  public StoreItem addStore(StoreItem newStore) {
+    storeItemRepository.save(newStore);
+    return newStore
+  }
+
+  public String deleteStore(String id) {
+    if(storeItemRepository.existsById(id) ) {
+      storeItemRepository.deleteById(id);
+      return "deleted id: " + id;
+    }
+    return "id does not exist: " + id;
   }
 }
