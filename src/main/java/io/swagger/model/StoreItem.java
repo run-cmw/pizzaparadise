77ce1b9f057023181b074906566b65a6ef79eeb4
Clara Mae Wells <wells.cl@husky.neu.edu>
2019-10-22 15:03:35 -0700

    change toString(), add more robust testing

--- src/main/java/io/swagger/model/StoreItem.java
+++ src/main/java/io/swagger/model/StoreItem.java
@@ -172,27 +172,20 @@ public class StoreItem {
     return Objects.hash(id, streetNumAndName, city, state, zipCode);
   }
 
+  /**
+   * String representation of StoreItem.
+   */
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("class StoreItem {\n");
 
-    sb.append("    id: ").append(toIndentedString(id)).append("\n");
-    sb.append("    streetNumAndName: ").append(toIndentedString(streetNumAndName)).append("\n");
-    sb.append("    city: ").append(toIndentedString(city)).append("\n");
-    sb.append("    state: ").append(toIndentedString(state)).append("\n");
-    sb.append("    zipCode: ").append(toIndentedString(zipCode)).append("\n");
+    sb.append("    id: ").append(id).append("\n");
+    sb.append("    streetNumAndName: ").append(streetNumAndName).append("\n");
+    sb.append("    city: ").append(city).append("\n");
+    sb.append("    state: ").append(state).append("\n");
+    sb.append("    zipCode: ").append(zipCode).append("\n");
     sb.append("}");
     return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
   }
 }
--- src/test/java/io/swagger/model/StoreItemTest.java
+++ src/test/java/io/swagger/model/StoreItemTest.java
@@ -1,7 +1,10 @@
 package io.swagger.model;
 
-import io.swagger.model.StoreItem;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,9 +15,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringBootTest
 public class StoreItemTest {
   StoreItem storeItem;
+  StoreItem same;
+  StoreItem different;
 
   @Before
   public void setUp() throws Exception {
+    storeItem = new StoreItem("5dae8e058980e20b64e28175", "555 Sunshine Ave", "Seattle", "California", "70806");
+    same = new StoreItem("5dae8e058980e20b64e28179", "555 Sunshine Blvd", "Clam Gulch", "Alaska", "94608");
+    different = new StoreItem("5dae8e058980e20b64e28199", "555 Sunshine Blvd", "Clam Gulch", "Alaska", "94608");
+    
     // Test setters within setup method
     storeItem.setId("5dae8e058980e20b64e28179");
     storeItem.setStreetNumAndName("555 Sunshine Blvd");
@@ -29,8 +38,8 @@ public class StoreItemTest {
   }
 
   @Test
-  public void getStreetNumAndNameest() {
-    assertEquals("555 Sunshine Blvd", storeItem.getZipCode());
+  public void getStreetNumAndNameTest() {
+    assertEquals("555 Sunshine Blvd", storeItem.getStreetNumAndName());
   }
 
   @Test
@@ -46,5 +55,34 @@ public class StoreItemTest {
   @Test
   public void getZipCodeTest() {
     assertEquals("94608", storeItem.getZipCode());
+  }
+
+  @Test
+  public void equalsTest() {
+    assertTrue(same.equals(storeItem));
+    assertTrue(same.equals(same));
+    assertFalse(different.equals(storeItem));
+    assertFalse(same.equals(2));
+    assertFalse(same.equals(null));
+  }
+
+  @Test
+  public void hashCodeTest() {
+    assertEquals(same.hashCode(), storeItem.hashCode());
+    assertNotEquals(different.hashCode(), storeItem.hashCode());
+  }
+
+  @Test
+  public void toStringTest() {
+    final String STORE_ITEM_AS_STRING =
+      "class StoreItem {\n"
+      + "    id: " + storeItem.getId() + "\n"
+      + "    streetNumAndName: " + storeItem.getStreetNumAndName() + "\n"
+      + "    city: " + storeItem.getCity() + "\n" 
+      + "    state: " + storeItem.getState() + "\n"
+      + "    zipCode: " + storeItem.getZipCode() + "\n"
+      + "}";
+
+    assertEquals(STORE_ITEM_AS_STRING, storeItem.toString());
   }
 }
