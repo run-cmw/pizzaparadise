59d3f2e03ef1eb92598c44778f0fa67c927233a6
yejeelee <lee.yej@husky.neu.edu>
2019-11-13 01:22:11 -0800

    deleting unnecessary lines from configuration

--- src/main/java/io/swagger/configuration/MongoDBConfiguration.java
+++ src/main/java/io/swagger/configuration/MongoDBConfiguration.java
@@ -1,7 +1,5 @@
 package io.swagger.configuration;
 
-import io.swagger.repository.SpecialItemRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -10,12 +8,9 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @Configuration
 public class MongoDBConfiguration implements CommandLineRunner {
 
-  @Autowired
-  private SpecialItemRepository specialItemRepository;
-
   @Override
   public void run(String... strings) throws Exception {
-    System.out.println(specialItemRepository.findAll());
+
   }
 
 }
