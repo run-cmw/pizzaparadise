a4642155d18a8fe86b3187eec3e2af84fe7a40af
yejeelee <lee.yej@husky.neu.edu>
2019-11-18 13:25:46 -0800

    modified pirceResponse and test

--- src/main/java/io/swagger/model/PriceResponse.java
+++ src/main/java/io/swagger/model/PriceResponse.java
@@ -13,16 +13,32 @@ public class PriceResponse {
   @JsonProperty("Currency")
   private String currency;
 
+  @JsonProperty("Message")
+  private String message;
+
   /**
    * Construct a new PriceResponse
-   * @param success true if successfully got the price, false otherwise.
+   * Only create this if its true
    * @param price price that was found.
    * @param currency currency that was found.
    */
-  public PriceResponse(boolean success, Double price, String currency) {
-    this.success = success;
+  public PriceResponse(Double price, String currency) {
+    this.success = true;
     this.price = price;
     this.currency = currency;
+    this.message = null;
+  }
+
+  /**
+   * Construct a new PriceResponse
+   * Only create this if its false
+   * @param message message given to PriceResponse.
+   */
+  public PriceResponse(String message) {
+    this.success = false;
+    this.price = null;
+    this.currency = null;
+    this.message = message;
   }
 
   /**
@@ -47,5 +63,13 @@ public class PriceResponse {
    */
   public boolean isSuccess() {
     return success;
+  }
+
+  /**
+   * Get message of this PriceResponse.
+   * @return message of this PriceResponse
+   */
+  public String getMessage() {
+    return this.message;
   }
 }
--- src/test/java/io/swagger/model/PriceResponseTest.java
+++ src/test/java/io/swagger/model/PriceResponseTest.java
@@ -1,40 +1,45 @@
 package io.swagger.model;
 
-import org.junit.Assert;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertNull;
+import static junit.framework.TestCase.assertTrue;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PriceResponseTest {
 
-  @Test
-  public void isSuccessTest() {
-    PriceResponse response1 = new PriceResponse(true, 5.00, "USD");
-    PriceResponse response2 = new PriceResponse(false, null, null);
-    Assert.assertEquals(response1.isSuccess(), true);
-    Assert.assertEquals(response2.isSuccess(), false);
-    Assert.assertNotEquals(response1.isSuccess(), false);
+  public PriceResponse response1;
+  public PriceResponse response2;
+
+  @Before
+  public void setUp() {
+    response1 = new PriceResponse(5.00, "USD");
+    response2 = new PriceResponse("CART_NOT_FOUND");
   }
 
   @Test
-  public void getPriceTest() {
-    PriceResponse response1 = new PriceResponse(true, 5.00, "USD");
-    PriceResponse response2 = new PriceResponse(false, null, null);
-    PriceResponse response3 = new PriceResponse(true, 50.00, "USD");
-    Assert.assertEquals(response1.getPrice(), (Double) 5.00);
-    Assert.assertEquals(response2.getPrice(), null);
-    Assert.assertNotEquals(response1.getPrice(), (Double) 50.00);
-    Assert.assertEquals(response3.getPrice(), (Double) 50.00);
+  public void TestIsSuccess() {
+    assertTrue(response1.isSuccess());
+    assertFalse(response2.isSuccess());
   }
 
   @Test
-  public void getCurrenctTest() {
-    PriceResponse response1 = new PriceResponse(true, 5.00, "USD");
-    PriceResponse response2 = new PriceResponse(false, null, null);
-    PriceResponse response4 = new PriceResponse(true, 5.00, "EURO");
-    Assert.assertEquals(response1.getCurrency(), "USD");
-    Assert.assertEquals(response2.getCurrency(), null);
-    Assert.assertNotEquals(response1.getCurrency(), null);
-    Assert.assertNotEquals(response1.getCurrency(), "EURO");
-    Assert.assertEquals(response4.getCurrency(), "EURO");
+  public void TestGetPrice() {
+    assertEquals((Double) 5.00, response1.getPrice());
+    assertNull(response2.getPrice());
+  }
+
+  @Test
+  public void TestGetCurrency() {
+    assertEquals("USD", response1.getCurrency());
+    assertNull(response2.getCurrency());
+  }
+
+  @Test
+  public void TestGetMessage() {
+    assertEquals("CART_NOT_FOUND", response2.getMessage());
+    assertNull(response1.getMessage());
   }
 
 }
