2e6519f2a3f9def59c7fac480eacdfa8cc1a0408
yejeelee <lee.yej@husky.neu.edu>
2019-11-18 13:19:19 -0800

    seperated all constant Strings

---
+++ src/main/java/io/swagger/Message.java
@@ -0,0 +1,13 @@
+package io.swagger;
+
+public class Message {
+  public static final String TOO_MANY_PIZZA_TOPPINGS = "TOO_MANY_PIZZA_TOPPINGS";
+  public static final String STORE_NOT_FOUND = "STORE_NOT_FOUND";
+  public static final String PIZZA_SIZE_NOT_FOUND = "PIZZA_SIZE_NOT_FOUND";
+  public static final String STORE_CANNOT_SERVE_GLUTEN_FREE = "STORE_CANNOT_SERVE_GLUTEN_FREE";
+  public static final String SIDE_NOT_FOUND = "SIDE_NOT_FOUND";
+  public static final String TOPPING_NOT_FOUND = "TOPPING_NOT_FOUND";
+  public static final String CART_NOT_FOUND = "CART_NOT_FOUND";
+  public static final String USD_CURRENCY = "USD_CURRENCY";
+
+}
