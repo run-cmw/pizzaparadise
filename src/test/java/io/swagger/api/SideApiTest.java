945976b003b9e61650121839d7d14aeeb18154a4
Alexander Lash <abl@users.noreply.github.com>
2019-11-22 22:50:11 -0800

    First API test

--- src/test/java/io/swagger/api/SideApiTest.java
+++ src/test/java/io/swagger/api/SideApiTest.java
@@ -1,82 +1,63 @@
 package io.swagger.api;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
 
 import io.swagger.model.SideItem;
 import io.swagger.repository.SideItemRepository;
 import io.swagger.service.SideService;
+
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
-@TestPropertySource(locations = "classpath:/application-test.properties")
+@TestPropertySource("classpath:/application-test.properties")
+@EnableAutoConfiguration(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
+@SpringBootTest(classes = { SideApiController.class, SideService.class })
 public class SideApiTest {
-  final String TEST_SIDE_ID = "cheeseSticks";
+    @Autowired
+    private SideApi sideApi;
 
-  @Autowired private SideService sideService;
+    @MockBean
+    private SideItemRepository sideItemRepository;
+    private SideItem testSide;
+    private SideItem testSide2;
+    private SideItem testSide3;
 
-  @MockBean private SideItemRepository sideItemRepository;
-  private SideItem testSide;
-  private SideItem testSide2;
-  private SideItem testSide3;
+    @Before
+    public void setUp() {
+        testSide = new SideItem("cheesesticks", "Cheesesticks", 7.99, "appetizer");
+        testSide2 = new SideItem("chocolateChipCookie", "Chocolate chip cookie", 1.99, "dessert");
+        testSide3 = new SideItem("brownie", "Brownie", 2.49, "dessert");
 
-  @Before
-  public void setUp() {
-    testSide = new SideItem(TEST_SIDE_ID, "Cheesesticks", 7.99, "appetizer");
-    testSide2 = new SideItem("chocolateChipCookie", "Chocolate chip cookie", 1.99, "dessert");
-    testSide3 = new SideItem("brownie", "Brownie", 2.49, "dessert");
+        when(sideItemRepository.findAll())
+                .thenReturn(Stream.of(testSide, testSide2, testSide3).collect(Collectors.toList()));
 
-    when(sideItemRepository.findAll())
-        .thenReturn(Stream.of(testSide, testSide2, testSide3).collect(Collectors.toList()));
-
-    when(sideItemRepository.findById(TEST_SIDE_ID)).thenReturn(Optional.of(testSide));
-  }
-
-  @Test
-  public void getAllSidesTest() {
-    // Test size of list of sides
-    assertEquals(3, sideService.getAllSides().size());
-  }
-
-  @Test
-  public void getSideByIdTest() {
-
-    for (SideItem item : sideService.getAllSides()) {
-      System.err.println(item.toString());
+        when(sideItemRepository.findById("cheesesticks")).thenReturn(Optional.of(testSide));
     }
 
-    final SideItem actualSide = sideService.getSideById(TEST_SIDE_ID);
-
-    assertEquals(testSide, actualSide);
-    assertEquals(testSide.toString(), actualSide.toString());
-  }
-
-  @Test
-  public void getSideByIncorrectIdTest() {
-    final String NONEXISTENT_ID = "1989";
-
-    assertNull(sideService.getSideById(NONEXISTENT_ID));
-  }
-
-  @Test
-  public void equalityTest() {
-    assertEquals(testSide, testSide);
-    assertNotEquals(testSide, testSide2);
-    assertNotEquals(testSide, null);
-  }
+    @Test
+    public void testGetAllSides() {
+        final ResponseEntity<List<SideItem>> response = sideApi.getAllSides();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(3, response.getBody().size());
+    }
 }
