4ba1ca5bd58cc44db94100125f283301465a1994
Alexander Lash <abl@users.noreply.github.com>
2019-11-22 22:16:19 -0800

    Move specials to path variables

--- src/main/java/io/swagger/api/ApplySpecialApi.java
+++ src/main/java/io/swagger/api/ApplySpecialApi.java
@@ -4,27 +4,27 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
 import io.swagger.model.ApplySpecialResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @javax.annotation.Generated(
     value = "io.swagger.codegen.v3.generators.java.SpringCodegen",
     date = "2019-09-26T03:54:46.062Z[GMT]")
 @Api(value = "applySpecial", description = "Apply Special API")
-
 /** Interface for the Apply Special API */
 public interface ApplySpecialApi {
   /**
    * Apply the specified special to the given store and cart.
    *
    * @param specialId id of the special being requested
-   * @param cartId id of the cart receiving the special
    * @param storeId id of the store that the cart belongs to
+   * @param cartId id of the cart receiving the special
    * @return Success message that pizza was added to cart or error message if special is not valid
    *     for cart.
    */
   ResponseEntity<ApplySpecialResponse> applySpecial(
       @ApiParam(value = "Special to add") @RequestParam(required = true) String specialId,
-      @ApiParam(value = "Store that contains cart") @RequestParam(required = true) String storeId,
-      @ApiParam(value = "Cart to update") @RequestParam(required = true) String cartId)
+      @ApiParam(value = "Store that contains cart") @PathVariable("storeId") String storeId,
+      @ApiParam(value = "Cart to update") @PathVariable("cartId") String cartId)
       throws Exception;
 }
