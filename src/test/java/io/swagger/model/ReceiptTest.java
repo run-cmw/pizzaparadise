eb998580e931f1a5dd50765ba0c3aab01f89b660
yejeelee <lee.yej@husky.neu.edu>
2019-11-27 01:26:41 -0800

    modified receipt model test

--- src/test/java/io/swagger/model/ReceiptTest.java
+++ src/test/java/io/swagger/model/ReceiptTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.GregorianCalendar;
+
 import org.bson.types.ObjectId;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,11 +15,13 @@ public class ReceiptTest {
   private Receipt receipt;
   private Cart cart;
   private Card card;
+  private GregorianCalendar time;
 
   @Before
   public void setUp() {
     cart = makeCart();
     card = makeCard();
+    time = new GregorianCalendar();
     receipt = new Receipt(cart, card);
   }
 
@@ -33,6 +37,49 @@ public class ReceiptTest {
   @Test
   public void getReceiptId() {
     assertNotNull(receipt.getReceiptId());
+  }
 
+  @Test
+  public void getTimePlacedTest() {
+    Receipt sameReceipt = receipt;
+    assertNotNull(receipt.getTimePlaced());
+    assertEquals(sameReceipt.getTimePlaced(), receipt.getTimePlaced());
+  }
+
+  @Test
+  public void getCartTest() {
+    assertEquals(cart, receipt.getCart());
+  }
+
+  @Test
+  public void getCardTest() {
+    assertEquals(card, receipt.getCard());
+  }
+
+  @Test
+  public void toStringTest() {
+    String test = "Receipt{id=" + receipt.getReceiptId() + ", " + "Time placed=" + receipt.getTimePlaced() + ", "
+        + "StoreId=" + cart.getStoreID() + ", " + "Pizzas=" + cart.getPizzas() + ", " + "Sides=" + cart.getSides()
+        + ", " + "Total Amount=" + cart.getTotalAmount() + ", " + "Card=" + card.getCardProvider()
+        + "card number ending with " + card.getCardNumber().substring(this.card.getCardNumber().length() - 5) + "}";
+    assertEquals(test, receipt.toString());
+  }
+
+  @Test
+  public void equalsTest() {
+    assertEquals(receipt, receipt);
+    assertNotEquals(receipt, cart);
+    assertNotEquals(receipt, null);
+    Receipt sameReceipt = receipt;
+    assertEquals(receipt, sameReceipt);
+    Card card1 = new Card("YeJee", "Lee", "4400616718352235", 2, 2021, CardProvider.VISA);
+    Receipt receipt1 = new Receipt(cart, card1);
+    assertNotEquals(receipt, receipt1);
+    ObjectId id = new ObjectId();
+    Cart cart1 = new Cart("brooklyn", id);
+    Receipt receipt2 = new Receipt(cart1, card);
+    assertNotEquals(receipt, receipt2);
+    Receipt receipt3 = new Receipt(cart, card);
+    assertNotEquals(receipt, receipt3);
   }
 }
